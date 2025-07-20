0cd5aacd47e2993a88bcf515e66c7a3d37f7dd11
Victor <psygreg@pm.me>
2025-07-20 12:01:34 -0300

    improved installation for lsfg-vk, add support to Prism/Minecraft

--- scripts/extras.sh
+++ scripts/extras.sh
@@ -182,12 +182,75 @@ lsw_in () {
 }
 
 # install lsfg-vk
+# set up flatpak enabler function
+flatpak_enabler () {
+
+    # initialize variables with locations
+    local DLL_ABSOLUTE_PATH=$(dirname "$(realpath "$DLL_FIND")")
+    local ESCAPED_DLL_PATH=$(printf '%s\n' "$DLL_ABSOLUTE_PATH" | sed 's/[&/\]/\\&/g')
+    local CONF_LOC="${HOME}/.config/lsfg-vk/conf.toml"
+    # check if config exists, and if not, gets a model file
+    if [ ! -f "$CONF_LOC" ]; then
+        # make sure target dir exists
+        mkdir -p ${HOME}/.config/lsfg-vk/
+        wget https://raw.githubusercontent.com/PancakeTAS/lsfg-vk/refs/heads/develop/conf.toml
+        mv conf.toml ${HOME}/.config/lsfg-vk/
+    fi
+    # register dll location in config file
+    sed -i -E "s|^# dll = \".*\"|dll = \"$ESCAPED_DLL_PATH\"|" ${HOME}/.config/lsfg-vk/conf.toml
+    # apply flatpak overrides -- Lutris has permission for /home, so won't need any, but still needs the symlinks
+    _flatpaks=(com.heroicgameslauncher.hgl com.valvesoftware.Steam net.lutris.Lutris org.prismlauncher.PrismLauncher com.mojang.Minecraft)
+    for flat in "${_flatpaks[@]}"; do
+        if flatpak list | grep -q "$flat"; then
+            APP_DIR="$HOME/.var/app/$flat"
+            # overrides for AUR/CachyOS packages
+            if pacman -Qi lsfg-vk 2>/dev/null 1>&2; then
+                flatpak override \
+                  --user \
+                  --filesystem="/usr/lib/liblsfg-vk.so:ro" \
+                  --filesystem="/etc/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json:ro" \
+                  --filesystem="xdg-config/lsfg-vk:ro" \
+                  --filesystem="$DLL_ABSOLUTE_PATH:ro" \
+                  "$flat"
+            # overrides for install script
+            elif [ -f "$HOME/.local/lib/liblsfg-vk.so" ] && [ -f "$HOME/.local/share/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json" ]; then
+                flatpak override \
+                  --user \
+                  --filesystem="$HOME/.local/lib/liblsfg-vk.so:ro" \
+                  --filesystem="xdg-data/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json:ro" \
+                  --filesystem="xdg-config/lsfg-vk:ro" \
+                  --filesystem="$DLL_ABSOLUTE_PATH:ro" \
+                  "$flat"
+            fi
+            # set up directories for symlinks
+            mkdir -p "$APP_DIR/lib"
+            mkdir -p "$APP_DIR/config/vulkan/implicit_layer.d/"
+            mkdir -p "$APP_DIR/.config/lsfg-vk/"
+            # symlinks for AUR/CachyOS packages
+            if pacman -Qi lsfg-vk 2>/dev/null 1>&2; then
+                ln -sf "/usr/lib/liblsfg-vk.so" "$APP_DIR/lib/liblsfg-vk.so"
+                ln -sf "/etc/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json" "$APP_DIR/config/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json"
+                ln -sf "$HOME/.config/lsfg-vk/conf.toml" "$APP_DIR/.config/lsfg-vk/conf.toml"
+            # symlinks for installation script -- elif so it only creates the symlinks if files exist at the expected locations
+            elif [ -f "$HOME/.local/lib/liblsfg-vk.so" ] && [ -f "$HOME/.local/share/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json" ]; then
+                ln -sf "$HOME/.local/lib/liblsfg-vk.so" "$APP_DIR/lib/liblsfg-vk.so"
+                ln -sf "$HOME/.local/share/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json" "$APP_DIR/config/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json"
+                ln -sf "$HOME/.config/lsfg-vk/conf.toml" "$APP_DIR/.config/lsfg-vk/conf.toml"
+            fi
+            echo "Usage enabled successfully for $flat."
+        fi
+    done
+
+}
+
 lsfg_vk_in () {
 
     local title="LSFG-VK"
     local msg="$msg251"
     _msgbox_
     if whiptail --title "LSFG-VK" --yesno "$msg250" 12 78; then
+        # add check for DLL location
+        DLL_FIND=$(find $HOME -name Lossless.dll | head -n 1) || local title="LSFG-VK" && local msg="Lossless.dll not found. Did you install Lossless Scaling?" && _msgbox_ && return 1
         curl -sSf https://pancake.gay/lsfg-vk.sh | sh
         if [ $? -eq 0 ]; then
             # Ask user for path to Lossless.dll
@@ -197,20 +260,9 @@ lsfg_vk_in () {
                 return 1
             fi
             # check flatpaks
-            declare -a APP_NAME=(com.valvesoftware.Steam com.heroicgameslauncher.hgl net.lutris.Lutris)
-            for APP in "${APP_NAME[@]}"; do
-                if flatpak info "$APP" &>/dev/null; then
-                    APP_DIR="$HOME/.var/app/$APP"
-                    # Make directories in Flatpak location
-                    mkdir -p "$APP_DIR/lib"
-                    mkdir -p "$APP_DIR/data/Steam/steamapps/common/Lossless Scaling/"
-                    mkdir -p "$APP_DIR/config/vulkan/implicit_layer.d/"
-
-                    cp -v ~/.local/lib/liblsfg-vk.so "$APP_DIR/lib/" && echo
-                    cp -v "$DLL_PATH" "$APP_DIR/data/Steam/steamapps/common/Lossless Scaling/" && echo
-                    cp -v ~/.local/share/vulkan/implicit_layer.d/VkLayer_LS_frame_generation.json "$APP_DIR/config/vulkan/implicit_layer.d/" && echo
-                fi
-            done
+            if command -v flatpak &> /dev/null; then
+                flatpak_enabler
+            fi
             local title="LSFG-VK"
             local msg="$msg249"
             _msgbox_
