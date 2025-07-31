5ef8410f702fe839000be69ca7958a146cc76755
Victor <psygreg@pm.me>
2025-07-31 09:11:05 -0300

    cleanup build script

--- build-appimage.sh
+++ build-appimage.sh
@@ -16,7 +16,7 @@ cp -f linuxtoys-atom.lib appimagebuild/LinuxToys-Atom.AppDir/usr/bin/
 cp -f src/lang/* appimagebuild/LinuxToys-Atom.AppDir/usr/bin/
 
 # fetch dependencies
-cp /usr/bin/curl /usr/bin/wget /usr/bin/git /usr/bin/zenity /usr/bin/bash /usr/bin/ca-certificates appimagebuild/LinuxToys-Atom.AppDir/usr/bin/
+cp /usr/bin/curl /usr/bin/wget /usr/bin/git /usr/bin/zenity /usr/bin/bash appimagebuild/LinuxToys-Atom.AppDir/usr/bin/
 cp /usr/bin/git-* appimagebuild/LinuxToys-Atom.AppDir/usr/bin/
 cp /etc/ssl/certs/ca-certificates.crt appimagebuild/LinuxToys-Atom.AppDir/etc/ssl/certs/
 
