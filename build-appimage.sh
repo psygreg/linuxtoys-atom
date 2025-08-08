5ac4359189b4bfe54b1194190bb66602533678da
Victor <psygreg@pm.me>
2025-08-08 13:59:54 -0300

    fix appimage

--- build-appimage.sh
+++ build-appimage.sh
@@ -16,20 +16,42 @@ cp -f linuxtoys-atom.lib appimagebuild/LinuxToys-Atom.AppDir/usr/bin/
 cp -f src/lang/* appimagebuild/LinuxToys-Atom.AppDir/usr/bin/
 
 # fetch dependencies
-cp -u /usr/bin/curl /usr/bin/wget /usr/bin/git /usr/bin/zenity /usr/bin/bash /usr/lib/libwget.so.2 appimagebuild/LinuxToys-Atom.AppDir/usr/bin/
+cp -u /usr/bin/curl /usr/bin/wget /usr/bin/git /usr/bin/zenity /usr/bin/bash appimagebuild/LinuxToys-Atom.AppDir/usr/bin/
 cp -u /usr/bin/git-* appimagebuild/LinuxToys-Atom.AppDir/usr/bin/
 cp -u /etc/ssl/certs/ca-certificates.crt appimagebuild/LinuxToys-Atom.AppDir/etc/ssl/certs/
+
+# excluding libraries that may break packaging
+readarray -t exclude_list < appimagebuild/exclude.list
+should_exclude() {
+    local lib_name="$1"
+    local base_name=$(basename "$lib_name")
+    
+    for excluded in "${exclude_list[@]}"; do
+        # skip empty lines and comments
+        [[ -z "$excluded" || "$excluded" =~ ^[[:space:]]*# ]] && continue
+        # check if the basename matches the excluded name
+        if [[ "$base_name" == "$excluded" ]]; then
+            return 0  # should exclude (true)
+        fi
+    done
+    return 1  # should not exclude (false)
+}
 
 # fetch libraries for dependencies
 for bin in curl wget git bash zenity; do
     for dep in $(ldd /usr/bin/$bin | awk '{if ($3 ~ /^\//) print $3}'); do
-        cp -u --parents "$dep" appimagebuild/LinuxToys-Atom.AppDir/usr/lib64/
+        if ! should_exclude "$dep"; then
+            cp -u --parents "$dep" appimagebuild/LinuxToys-Atom.AppDir/usr/lib64/
+        fi
     done
 done
 # libwget fix
+cp -u /usr/lib/libwget.so.2 appimagebuild/LinuxToys-Atom.AppDir/usr/lib64/lib/x86_64-linux-gnu/
 for lib in /usr/lib/libwget.so.2; do
     for dep in $(ldd $lib | awk '{if ($3 ~ /^\//) print $3}'); do
-        cp -u --parents "$dep" appimagebuild/LinuxToys-Atom.AppDir/usr/lib64/
+        if ! should_exclude "$dep"; then
+            cp -u --parents "$dep" appimagebuild/LinuxToys-Atom.AppDir/usr/lib64/
+        fi
     done
 done
 
@@ -39,7 +61,9 @@ cp -u /usr/lib/git-core/git-* appimagebuild/LinuxToys-Atom.AppDir/usr/lib64/lib/
 # get git helpers dependencies
 for helper in /usr/lib/git-core/*; do
     for hldep in $(ldd $helper | awk '{if ($3 ~ /^\//) print $3}'); do
-        cp -u --parents "$hldep" appimagebuild/LinuxToys-Atom.AppDir/usr/lib64/
+        if ! should_exclude "$hldep"; then
+            cp -u --parents "$hldep" appimagebuild/LinuxToys-Atom.AppDir/usr/lib64/
+        fi
     done
 done
 # cp -u /usr/lib/x86_64-linux-gnu/libcurl-gnutls.so.4 appimagebuild/LinuxToys-Atom.AppDir/usr/lib64/lib/x86_64-linux-gnu/
